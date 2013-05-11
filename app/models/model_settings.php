89efd41d3473f9cb0af2fc5980c95d1606898b4f
dlozupone <dlozupone@e93f5933-011e-4631-b18b-ff0f5bd0dab4>
2013-05-11 11:45:35 +0000

    added get_storage_settings method

--- app/models/model_settings.php
+++ app/models/model_settings.php
@@ -25,7 +25,7 @@ if ( ! class_exists( WP_Models_Settings_Model ) ):
 		public function __construct()
 		{
 			$this->settings = array(
-				'use_amazon' => true,
+				'use_amazon' => false,
 				'amazon_bucket' => 'wp-models',
 				'amazon_accessKeyId' => 'AKIAJ7QBMH6DECYGW7WQ',
 				'amazon_secret' => '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',
@@ -37,6 +37,27 @@ if ( ! class_exists( WP_Models_Settings_Model ) ):
 		public function get_settings()
 		{
 			return $this->settings;
+		}
+		
+		public function get_storage_settings()
+		{
+			if( $this->settings['use_amazon'] ):
+ 				$settings = array(
+	 				'location' => 'amazonS3',
+	 				'access_key' => $this->settings['amazon_accessKeyId'],
+	 				'secret_key' => $this->settings['amazon_secret'],
+	 				'bucket' =>$this->settings['amazon_bucket']
+ 				);
+	 		else:
+	 			$settings = array(
+		 			'location' => 'local',
+		 			'access_key' => null,
+		 			'secret_key' => null,
+		 			'bucket' => null
+	 			);
+	 		endif;
+	 		
+	 		return $settings;
 		}
 	}
 endif;
