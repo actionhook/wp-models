31529f5ffeda548ca5f4ab381bff5570174be5df
dlozupone <dlozupone@e93f5933-011e-4631-b18b-ff0f5bd0dab4>
2013-05-11 11:39:47 +0000

    Extensive changes to plugin_controller.php

--- app/controllers/plugin_controller.php
+++ app/controllers/plugin_controller.php
@@ -27,18 +27,24 @@ if ( ! class_exists( WP_Models ) ):
 	 	 */
 	 	public function init()
 	 	{
-	 		//get the plugin settings
-	 		require_once( $this->models_path . '/model_settings.php' );
-	 		$settings = new WP_Models_Settings_Model();
-	 		$this->settings = $settings->get_settings();
-			
-	 		//set up the plugin custom post types
+	 		//require necessary files
 	 		require_once( $this->models_path . '/model_cpt_models.php' );
 	 		require_once( $this->models_path . '/model_cpt_shoots.php' );
+	 		
+	 		define( '_WP_MODELS_CPT_MODELS_SLUG', WP_Models_CPT_Models_Model::get_slug() );
+	 		define( '_WP_MODELS_CPT_SHOOTS_SLUG', WP_Models_CPT_Shoots_Model::get_slug() );
+	 			
+	 		//get the plugin settings
+	 		require_once( $this->models_path . '/model_settings.php' );
+	 		$this->settings = new WP_Models_Settings_Model();
+			
+	 		//set up the plugin custom post types
 	 		$this->cpts = array(
-	 			'models' => new WP_Models_CPT_Models_Model( $this->txtdomain ),
-	 			'shoots' => new WP_Models_CPT_Shoots_Model( $this->txtdomain )
+	 			_WP_MODELS_CPT_MODELS_SLUG => new WP_Models_CPT_Models_Model( $this->uri, $this->txtdomain ),
+	 			_WP_MODELS_CPT_SHOOTS_SLUG => new WP_Models_CPT_Shoots_Model( $this->uri, $this->txtdomain )
 	 		);
+	 		
+	 		//print_r($this->cpts);
 	 		
 	 		//setup our nonce name and action
 	 		$this->nonce_name = '_wp_models_nonce';
@@ -48,16 +54,19 @@ if ( ! class_exists( WP_Models ) ):
 	 		add_filter( 'filter_metabox_callback_args', array( &$this, 'setup_metabox_args' ), 10, 2 );
 	 		
 	 		//add our ajax callbacks
-	 		add_action( 'wp_ajax_wp_models_ajax_media_upload', array( &$this, 'ajax_media_upload' ) );
-	 		add_action( 'wp_ajax_wp_models_shoot_media', array( &$this, 'ajax_get_shoot_media' ) );
-	 		add_action( 'wp_ajax_wp_models_delete_shoot_pic', array( &$this, 'ajax_delete_shoot_media' ) );
-	 		add_action( 'wp_ajax_wp_models_delete_shoot_vid', array( &$this, 'ajax_delete_shoot_media' ) );
+	 		add_action( 'wp_ajax_wp_models_media_upload', 	array( &$this, 'ajax_media_upload' ) );
+	 		add_action( 'wp_ajax_wp_models_get_media', 		array( &$this, 'ajax_get_media' ) );
+	 		add_action( 'wp_ajax_wp_models_delete_shoot_pic', 	array( &$this, 'ajax_delete_media' ) );
+	 		add_action( 'wp_ajax_wp_models_delete_shoot_vid', 	array( &$this, 'ajax_delete_media' ) );
 	 		
 	 		//filter js l10n as necessary
-	 		add_filter( 'filter_script_localizations', array( &$this, 'filter_shoot_cpt_admin_js' ), 10, 2 );
+	 		add_filter( 'filter_admin_scripts_l10n_args-wp-models-cpt-shoots-admin',	array( &$this, 'filter_shoot_cpt_admin_js' ), 10 );
 	 		
 	 		// Support the file format webm mimetype
 			add_filter( 'upload_mimes', array( &$this, 'custom_mimes' ) );
+			
+			//add our content filters
+			add_filter( 'the_content',	array( &$this, 'render_model_single' ) );
 	 	}
 	 	
 	 	/**
@@ -75,7 +84,7 @@ if ( ! class_exists( WP_Models ) ):
 	 		switch( $metabox['id'] )
 	 		{
 	 			case 'wp-models-shoot-models':
-	 				$models = $this->cpts['models']->get_models();
+	 				$models = $this->cpts[_WP_MODELS_CPT_MODELS_SLUG]->get_models();
 	 				foreach( $models as $model ):
 	 					$metabox['args']['models'][$model->ID] = $model->post_title;
 	 				endforeach;
@@ -97,11 +106,11 @@ if ( ! class_exists( WP_Models ) ):
 	 	{
 	 		//check for security
 	 		if ( ! isset( $_POST['nonce'] ) || ! check_ajax_referer( $this->nonce_name, 'nonce' ) )
-	 			die();
-	 		
-	 		$this->cpts['shoots']->save_shoot_media( $_POST, $_FILES, true );
+	 			die( 'NONCE CHECK FAILED' );
+		
+	 		$result = $this->cpts[$_POST['post_type']]->save_media( $_POST, $_FILES, true );
 	 		print_r( $_POST);
-	 		die();
+	 		die( $result );
 	 	}
 	 	
 	 	/**
@@ -111,40 +120,45 @@ if ( ! class_exists( WP_Models ) ):
 	 	 *
 	 	 * @since 0.1
 	 	 */
-	 	public function ajax_get_shoot_media()
+	 	public function ajax_get_media()
 	 	{
+	 	
+	 		//print_r( $_POST );
 	 		//configure the get_shoot_media parameters and include required files based on storage location
 	 		/**
 	 		 * @todo change settings to use storage_location
 	 		 */
-	 		if( $this->settings['use_amazon'] ):
+	 		 
+	 		$settings = $this->settings->get_storage_settings();
+	 		
+	 		if( $settings['location'] == 'amazonS3' )
 	 			require_once( trailingslashit( $this->path) . 'lib/s3.php' );
- 				$location = 'amazonS3';
- 				$access_key = $this->settings['amazon_accessKeyId'];
- 				$secret_key = $this->settings['amazon_secret'];
- 				$bucket = $this->settings['amazon_bucket'];
-	 		else:
-	 			$location = 'local';
-	 			$access_key = null;
-	 			$secret_key = null;
-	 			$bucket = null;
-	 		endif;
 			
-			//get the shoot media
-			$shoot_media = $this->cpts['shoots']->get_shoot_media( $_POST['post'], $_POST['type'], $location, $access_key, $secret_key, $bucket );
+			//get the post media
+			$post_media = $this->cpts[$_POST['post_type']]->get_media( 
+				$_POST['post'],
+				$_POST['media_type'],
+				$settings['location'],
+				$settings['access_key'],
+				$settings['secret_key'],
+				$settings['$bucket']
+			);
 			
-			//if we have an array of photos, include the view
-	 		if ( is_array( $shoot_media ) ):
+			//set variables for the template
+			$uri = $this->uri;
+			
+			//if we have an array of media items, include the appropriate view
+	 		if ( is_array( $post_media ) ):
 		 		ob_start();
-		 		require_once( trailingslashit( $this->views_path ) . 'admin_ajax_shoot_'. $_POST['type'] . '_html.php' );
+		 		require_once( trailingslashit( $this->views_path ) . 'admin_ajax_'. $_POST['media_type'] . '_html.php' );
 		 		$html = ob_get_clean();
 	 		endif;
 	 		
 	 		if ( $html == '' ):
-	 			if ( $_POST['type'] == 'pics' ):
-	 				$html = __( 'There are no pictures associated with this shoot.', $this->txtdomain );
+	 			if ( $_POST['media_type'] == 'pics' ):
+	 				$html = __( 'There are no pictures associated with this .', $this->txtdomain );
 	 			else:
-	 				$html = __( 'There are no videos associated with this shoot.', $this->txtdomain );
+	 				$html = __( 'There are no videos associated with this .', $this->txtdomain );
 	 			endif;
 	 		endif;
 	 		
@@ -158,20 +172,22 @@ if ( ! class_exists( WP_Models ) ):
 	 	 *
 	 	 * @since 0.1
 	 	 */
-	 	public function ajax_delete_shoot_media()
+	 	public function ajax_delete_media()
 	 	{
 	 		//check for security
 	 		if ( ! isset( $_POST['nonce'] ) || ! check_ajax_referer( $this->nonce_name, 'nonce' ) )
 	 			die( 'Security check failed' );
 	 				
-	 		print_r( $_POST );
+	 		//print_r( $_POST );
 	 		if ( $_POST['action'] == 'wp_models_delete_shoot_pic' ):
 	 			$type = 'pics';
 	 		elseif ( $_POST['action'] == 'wp_models_delete_shoot_vid' ):
 	 			$type = 'vids';
 	 		endif;
-	 			
-	 		$result = $this->cpts['shoots']->delete_shoot_media( $_POST['post_id'], $_POST['media'], $type );
+	 		
+	 		$settings = $this->settings->get_storage_settings();
+	 		
+	 		$result = $this->cpts[$_POST['post_type']]->delete_media( $_POST['post_id'], $_POST['media'], $type, $settings['location'] );
 	 		die( $result );
 	 	}
 	 	
@@ -198,18 +214,20 @@ if ( ! class_exists( WP_Models ) ):
 		 * @param array $args Contains key/value pairs of script localizations.
 		 * @since 0.1
 		 */
-		public function filter_shoot_cpt_admin_js( $handle, $args )
+		public function filter_shoot_cpt_admin_js( $args )
 		{
-			if ( $handle == 'wp-models-cpt-shoots-admin' && $this->settings['use_amazon'] ):
+			$settings = $this->settings->get_storage_settings();
+			
+			if ( $settings['location'] == 'amazonS3' ):
 				require_once( trailingslashit( $this->path ) . 'lib/class-s3.php' );
 				
 				//get the Amazon S3 settings
-				$bucket = $this->settings['amazon_bucket'];
-				$accessKeyId = $this->settings['amazon_accessKeyId'];
-				$secret = $this->settings['amazon_secret'];
+				$bucket = $settings['bucket'];
+				$accessKeyId = $settings['accessKeyId'];
+				$secret = $settings['secret'];
 				//This is for setting either Standard or Reduced Redundancy Storage. Currently, STANDARD is always selected even when
 				//the value of this is REDUCED_REDUNDANCY
-				$storage_class = $this->settings['amazon_storage_class'];
+				$storage_class = $settings['storage_class'];
 				
 				$S3 = new S3_Helper_Functions( $bucket, $accessKeyId, $secret );
 				
@@ -219,7 +237,7 @@ if ( ! class_exists( WP_Models ) ):
 					'expiration' => date('Y-m-d\TH:i:s.000\Z', strtotime('+1 day')),  
 					'conditions' => array(
 						array('bucket' => $bucket),
-						array('acl' => 'public-read'),
+						array('acl' => 'private'),
 						array('starts-with', '$key', ''),
 						array('starts-with', '$Content-Type', ''),
 						// "Some versions of the Adobe Flash Player do not properly handle HTTP responses that have an empty body. 
@@ -250,9 +268,74 @@ if ( ! class_exists( WP_Models ) ):
 	 			$args['signature'] = $signature;
 			endif;
 			
+			//add the nonce for media uploads/deletes
+			$args['nonce'] = wp_create_nonce( $this->nonce_name );
+			
 			//return the modified args
 			return $args;
 		}
+		
+		/**
+		 * Render the single model page view.
+		 *
+		 * This view is rendered using the WP filter the_content. This is done to ensure compatibility with all themes and membership plugins.
+		 *
+		 * @package pkgtoken
+		 * @subpackage subtoken
+		 * @param string $content The WP post content.
+		 * @since 0.1
+		 */
+		public function render_model_single( $content )
+		{
+			global $post;
+			
+			if( is_single() && $post->post_type == $this->cpts[$post->post_type]->get_slug() ):
+				$settings = $this->settings->get_storage_settings();
+	 		
+		 		if( $settings['location'] == 'amazonS3' )
+		 			require_once( trailingslashit( $this->path) . 'lib/s3.php' );
+				
+				//get the post media
+				$post_pics = $this->cpts[$post->post_type]->get_media( 
+					$post->ID,
+					'pics',
+					$settings['location'],
+					$settings['access_key'],
+					$settings['secret_key'],
+					$settings['$bucket']
+				);
+				
+				$post_vids = $this->cpts[$post->post_type]->get_media( 
+					$post->ID,
+					'vids',
+					$settings['location'],
+					$settings['access_key'],
+					$settings['secret_key'],
+					$settings['$bucket']
+				);
+				
+				//get the model details
+				$meta = get_post_meta( $post->ID, $this->cpts[_WP_MODELS_CPT_MODELS_SLUG]->get_metakey(), true );
+				
+				//create the info string from meta keys/values
+				if( is_array( $meta ) ):
+					foreach( $meta as $key => $item ):
+						$model_info .= ucfirst( str_replace('model_', '', $key ) ) . ': ' . $item . ' ';			
+					endforeach;
+				endif;
+				
+				//allow the end user to filter the info line
+				$model_info = apply_filters( 'wp_models_filter_model_info', $model_info, $post->ID, $meta );
+				
+				//include the view
+				ob_start();
+				require_once( trailingslashit( $this->views_path ) . 'single_model.php' );
+				$content = ob_get_clean();
+			endif;
+			
+			return $content;
+		}
+		
 		
 		public static function activate()
 		{
