e0a549faecedf9a4b76e2ec17cbe5c627b697643
dlozupone <dlozupone@e93f5933-011e-4631-b18b-ff0f5bd0dab4>
2013-05-26 02:02:46 +0000

    modified shoot.png [#29 state:resolved] Added routine to set default settings [#21 state:resolved] Fixed single post templates

--- app/views/content-wp-models-shoot.php
+++ app/views/content-wp-models-shoot.php
@@ -8,29 +8,29 @@
  * @since WP-Models 0.1
  */
 ?>
-		<div class="wp-models-model-info">
+		<div class="wp-models-shoot-info">
 			<p>Age: <?php wp_models_model_age(); ?> | Height: <?php wp_models_model_height(); ?>  | Weight: <?php wp_models_model_weight(); ?>  | <?php wp_models_model_bust(); ?>-<?php wp_models_model_waist(); ?>-<?php wp_models_model_hips(); ?></p>
 		</div>
 		<div class="wp-models-toggle-container">
-			<div class="wp-models-model-content">
+			<div class="wp-models-shoot-content">
 				<?php wp_models_model_content(); ?>	
 			</div>
 		</div>
 		
 	<?php if( wp_models_have_pics() ): ?>
-		<div class="wp-models-model-pics">
+		<div class="wp-models-shoot-pics">
 			<h3><?php the_title(); ?> Pictures</h3>
 			<?php while ( wp_models_have_pics() ): wp_models_the_pic(); ?>
-				<a href="<?php echo wp_models_media_permalink(); ?>" class="wp-models-gallery" title="<?php the_title(); ?>"><img src="<?php echo wp_models_media_permalink(); ?>" class="wp-models-model-pic" /></a>
+				<a href="<?php echo wp_models_media_permalink(); ?>" class="wp-models-gallery" title="<?php the_title(); ?>"><img src="<?php echo wp_models_media_permalink(); ?>" class="wp-models-shoot-pic" /></a>
 			<?php endwhile; ?>
 			<div style="clear: both;"></div>
 		</div>
 	<?php endif; ?>
 	
 	<?php if( wp_models_have_vids() ):?>
-		<div class="wp-models-model-vids">
+		<div class="wp-models-shoot-vids">
 			<h3><?php the_title(); ?> Videos</h3>
-			<div class="wp-models-model-vid is-splash color-light">
+			<div class="wp-models-shoot-vid is-splash color-light">
 			<?php while ( wp_models_have_vids() ): wp_models_the_vid(); ?>
 				<video controls>
 					<source type="<?php wp_models_media_mimetype(); ?>" src="<?php echo wp_models_media_permalink(); ?>" />
