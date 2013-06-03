c17521272d472ee8203f47c938feb854ed1067d9
dlozupone <dlozupone@e93f5933-011e-4631-b18b-ff0f5bd0dab4>
2013-06-03 21:57:29 +0000

    Updated: admin settings page with marketing collateral Updated: admin settings page css

--- app/models/model_settings.php
+++ app/models/model_settings.php
@@ -63,7 +63,9 @@ if ( ! class_exists( 'WP_Models_Settings_Model' ) ):
 					'position'		=> '99.3141579',
 					'view'			=> 'admin_options.php',
 					'css'			=> array( 
-						array( 'handle' => 'wp-models-admin-settings', 'src' => $uri . 'css/wp-models-admin-settings.css', 'deps' => null, 'ver' => false, 'media' => 'all' )
+						array( 'handle' => 'wp-models-admin-settings', 'src' => $uri . 'css/wp-models-admin-settings.css', 'deps' => null, 'ver' => false, 'media' => 'all' ),
+						array( 'handle' => 'twitter-bootstrap', 'src' => '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.no-icons.min.css', 'deps' => null, 'ver' => false, 'media' => 'all' ),
+						array( 'handle' => 'font-awesome', 'src' => '//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css', 'deps'=> 'twiter-bootstrap', 'ver' => false, 'media' => 'all' )
 					),
 					'help_screen'	=> array(
 							new Base_Model_Help_Tab( __( 'Overview', $txtdomain ), 'wp-models-settings-help', null, null, $path . 'help_screen_settings_general.php' )
