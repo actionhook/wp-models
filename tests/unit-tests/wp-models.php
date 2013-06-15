40bc908c1ac244b2e5f514617f3ebb44f8ff3c7c
Daryl Lozupone <daryl@actionhook.com>
2013-06-14 23:40:49 -0400

    Added: Main plugin controller tests

--- tests/unit-tests/wp-models.php
+++ tests/unit-tests/wp-models.php
@@ -1,5 +1,4 @@
 <?php
-//use \WP_Unit_Tests;
 
 class wpModelsTests extends WP_UnitTestCase
 {
@@ -14,14 +13,13 @@ class wpModelsTests extends WP_UnitTestCase
 	public function setUp() {
 		parent::setUp();
 		
-		global $WP_Models;
-		$this->_plugin = $WP_Models;
-		
+		$this->_plugin = new WP_Models( 'wp-models', '1.0.2', WPM_PLUGIN_PATH , WPM_MAIN_PLUGIN_FILE, WPM_PLUGIN_URL, 'wp-models' );
 		$this->factory = new WPM_UnitTest_Factory();
 		
 		//create a new post and get the id
 		$post_id = $this->factory->post->create( array( 'post_title' => 'Test Model', 'post_type' => 'wp-models-model', 'post_status' => 'draft' ) );
 		
+		//generate some post meta
 		$meta = array(
 			'model_age' => 24,
 			'model_height' => "5' 7''",
@@ -33,12 +31,91 @@ class wpModelsTests extends WP_UnitTestCase
 		
 		update_post_meta( $post_id, ' _wp-models-model', $meta );
 		
+		//store the post as a class property
 		$this->_post = get_post( $post_id );
-		
-		//call this action to intialize the post object fully 
-		do_action( 'the_post', $this->_post );
 	}
 	
+	/**
+	 * verify base controller plugin exists
+	 *
+	 * @package pkgtoken
+	 * @since
+	 */
+	public function testBaseControllerPluginExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/base/controllers/base_controller_plugin.php' );
+	}
+	
+	/**
+	 * verify base controller plugin exists
+	 *
+	 * @package pkgtoken
+	 * @since
+	 */
+	public function testBaseModelExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/base/models/base_model.php' );
+	}
+	
+	public function testBaseModelCptExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/base/models/base_model_cpt.php' );
+	}
+	
+	public function testBaseModelHelpTabExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/base/models/base_model_help_tab.php' );
+	}
+	
+	public function testBaseModelJsObjectExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/base/models/base_model_js_object.php' );
+	}
+	
+	public function testBaseModelMetaboxExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/base/models/base_model_metabox.php' );
+	}
+	
+	public function testBaseModelSettingsExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/base/models/base_model_settings.php' );
+	}
+	
+	public function testBaseViewBaseOptionsPageExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/base/views/base_options_page.php' );
+	}
+	
+	public function testBaseViewBaseSettingsSectionExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/base/views/base_settings_section.php' );
+	}
+	
+	public function testPluginControllerExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/app/controllers/plugin_controller.php' );
+	}
+	
+	public function testHelperPluploadExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/app/helpers/plupload.php' );
+	}
+	
+	public function testModelCptModelsExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/app/models/model_cpt_models.php' );
+	}
+	
+	public function testModelSettingsExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/app/models/model_settings.php' );
+	}
+	
+	public function testModelStorageLocationExists()
+	{
+		$this->assertFileExists( WPM_PLUGIN_PATH . '/app/models/model_storage_location.php' );
+	}
 	/**
 	 * verify plugin initialization
 	 *
